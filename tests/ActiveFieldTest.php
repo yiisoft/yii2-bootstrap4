fc9908b3eec4c9cbffe9d87692ce652d3e57fc77
Evgenii Grekov <jgrekov@gmail.com>
2021-03-03 17:13:49 +0500

    Add tests for checkbox, radio, checkboxList, radioList

--- tests/ActiveFieldTest.php
+++ tests/ActiveFieldTest.php
@@ -89,6 +89,51 @@ HTML;
         $this->assertEqualsWithoutLE($expectedHtml, $html);
     }
 
+    public function testRadioError()
+    {
+        Html::$counter = 0;
+        $this->helperModel->addError($this->attributeName, 'Test print error message');
+        $html = $this->activeField->radio()->render();
+
+        $expectedHtml = <<<HTML
+<div class="form-group field-dynamicmodel-attributename">
+<div class="custom-control custom-radio">
+<input type="hidden" name="DynamicModel[attributeName]" value="0"><input type="radio" id="dynamicmodel-attributename" class="custom-control-input is-invalid" name="DynamicModel[attributeName]" value="1">
+<label class="custom-control-label" for="dynamicmodel-attributename">Attribute Name</label>
+<div class="invalid-feedback">Test print error message</div>
+
+</div>
+</div>
+HTML;
+        $this->assertEqualsWithoutLE($expectedHtml, $html);
+    }
+
+    public function testRadioListError()
+    {
+        Html::$counter = 0;
+        $this->helperModel->addError($this->attributeName, 'Test print error message');
+        $html = $this->activeField->radioList([1 => 'name1', 2 => 'name2'])->render();
+
+        $expectedHtml = <<<HTML
+<div class="form-group field-dynamicmodel-attributename">
+<label>Attribute Name</label>
+<input type="hidden" name="DynamicModel[attributeName]" value=""><div id="dynamicmodel-attributename" class="is-invalid" role="radiogroup" aria-invalid="true"><div class="custom-control custom-radio">
+<input type="radio" id="i5" class="custom-control-input is-invalid" name="DynamicModel[attributeName]" value="0">
+<label class="custom-control-label" for="i5">name1</label>
+</div>
+
+<div class="custom-control custom-radio">
+<input type="radio" id="i6" class="custom-control-input is-invalid" name="DynamicModel[attributeName]" value="1">
+<label class="custom-control-label" for="i6">name2</label>
+<div class="invalid-feedback">Test print error message</div>
+</div>
+</div>
+
+</div>
+HTML;
+        $this->assertEqualsWithoutLE($expectedHtml, $html);
+    }
+
     public function testCheckboxList()
     {
         Html::$counter = 0;
@@ -106,6 +151,51 @@ HTML;
 <input type="checkbox" id="i1" class="custom-control-input" name="DynamicModel[attributeName][]" value="2">
 <label class="custom-control-label" for="i1">name2</label>
 <div class="invalid-feedback"></div>
+</div>
+</div>
+
+</div>
+HTML;
+        $this->assertEqualsWithoutLE($expectedHtml, $html);
+    }
+
+    public function testCheckboxError()
+    {
+        Html::$counter = 0;
+        $this->helperModel->addError($this->attributeName, 'Test print error message');
+        $html = $this->activeField->checkbox()->render();
+
+        $expectedHtml = <<<HTML
+<div class="form-group field-dynamicmodel-attributename">
+<div class="custom-control custom-checkbox">
+<input type="hidden" name="DynamicModel[attributeName]" value="0"><input type="checkbox" id="dynamicmodel-attributename" class="custom-control-input is-invalid" name="DynamicModel[attributeName]" value="1">
+<label class="custom-control-label" for="dynamicmodel-attributename">Attribute Name</label>
+<div class="invalid-feedback">Test print error message</div>
+
+</div>
+</div>
+HTML;
+        $this->assertEqualsWithoutLE($expectedHtml, $html);
+    }
+
+    public function testCheckboxListError()
+    {
+        Html::$counter = 0;
+        $this->helperModel->addError($this->attributeName, 'Test print error message');
+        $html = $this->activeField->checkboxList([1 => 'name1', 2 => 'name2'])->render();
+
+        $expectedHtml = <<<HTML
+<div class="form-group field-dynamicmodel-attributename">
+<label>Attribute Name</label>
+<input type="hidden" name="DynamicModel[attributeName]" value=""><div id="dynamicmodel-attributename" class="is-invalid" aria-invalid="true"><div class="custom-control custom-checkbox">
+<input type="checkbox" id="i5" class="custom-control-input is-invalid" name="DynamicModel[attributeName][]" value="0">
+<label class="custom-control-label" for="i5">name1</label>
+</div>
+
+<div class="custom-control custom-checkbox">
+<input type="checkbox" id="i6" class="custom-control-input is-invalid" name="DynamicModel[attributeName][]" value="1">
+<label class="custom-control-label" for="i6">name2</label>
+<div class="invalid-feedback">Test print error message</div>
 </div>
 </div>
 
