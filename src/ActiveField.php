1a384417771d87de10e619f65bfd46973c59f759
Marco van 't Wout <marco@tremani.nl>
2024-02-15 16:36:42 +0100

    Fix name of method ActiveField::dropDownList()
    Same as in https://github.com/yiisoft/yii2-bootstrap5/pull/22

--- src/ActiveField.php
+++ src/ActiveField.php
@@ -415,13 +415,13 @@ class ActiveField extends \yii\widgets\ActiveField
     /**
      * {@inheritdoc}
      */
-    public function dropdownList($items, $options = [])
+    public function dropDownList($items, $options = [])
     {
         if ($this->form->layout === ActiveForm::LAYOUT_INLINE) {
             Html::removeCssClass($this->labelOptions, 'sr-only');
         }
 
-        return parent::dropdownList($items, $options);
+        return parent::dropDownList($items, $options);
     }
 
     /**
