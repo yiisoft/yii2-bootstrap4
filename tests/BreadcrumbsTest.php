a1bd942d2f481c3ff6c89e4158ae0e053b4078e6
Alexander Makarov <sam@rmcreative.ru>
2026-06-30 20:50:49 +0300

    Cover default breadcrumbs home link

--- tests/BreadcrumbsTest.php
+++ tests/BreadcrumbsTest.php
@@ -8,6 +8,9 @@
 namespace yiiunit\extensions\bootstrap4;
 
 use yii\bootstrap4\Breadcrumbs;
+use yii\bootstrap4\Widget;
+use yii\base\InvalidConfigException;
+use yii\web\Application;
 
 /**
  * @group bootstrap4
@@ -34,5 +37,42 @@ HTML;
 
 
         $this->assertEqualsWithoutLE($expected, $out);
+    }
+
+    public function testRenderWithDefaultHomeLink(): void
+    {
+        Breadcrumbs::$counter = 0;
+        $out = Breadcrumbs::widget([
+            'links' => [
+                ['label' => 'Library', 'url' => '#'],
+                ['label' => 'Data']
+            ]
+        ]);
+
+        $expected = <<<HTML
+<nav aria-label="breadcrumb"><ol id="w0" class="breadcrumb"><li class="breadcrumb-item"><a href="/index.php">Home</a></li>
+<li class="breadcrumb-item"><a href="#">Library</a></li>
+<li class="breadcrumb-item active" aria-current="page">Data</li>
+</ol></nav>
+HTML;
+
+        $this->assertEqualsWithoutLE($expected, $out);
+    }
+
+    public function testRenderRequiresWebApplication(): void
+    {
+        $this->destroyApplication();
+
+        $this->expectException(InvalidConfigException::class);
+        $this->expectExceptionMessage('Bootstrap widgets require Yii::$app to be an instance of yii\web\Application.');
+
+        $widget = new class () extends Widget {
+            public function getApplication(): Application
+            {
+                return $this->getApp();
+            }
+        };
+
+        $widget->getApplication();
     }
 }
